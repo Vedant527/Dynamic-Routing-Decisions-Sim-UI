5e0964e48d29736f81237e04478a0cde00e6c250
F655HDZ <rakesh.gorrepati@firstdata.com>
2020-08-05 17:03:05 -0400

    Reorganized Files, Took out Spring, Updated Name

--- src/drd/intern/project/Main.java
+++ src/drd/intern/project/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("main-stage.fxml"));
-        primaryStage.setTitle("Registration Form FXML Application");
+        primaryStage.setTitle("DRD-GUI");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
